ad0b5b55850f4942098916eafdfb9714d5f522c1
boubechtoula <dw_boubechtoula@esi.dz>
2019-10-26 20:52:01 +0100

    router add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule,RouterModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
@@ -7,10 +7,19 @@ import { HelloComponent } from './hello.component';
 import { TestComponent } from './test/test.component';
 import { AppareilComponent } from './appareil/appareil.component';
 import { AppareilService } from './services/appareil.service';
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'appareils', component: AppareilViewComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: '', component: AppareilViewComponent }
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TestComponent, AppareilComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(appRoutes) ],
+  declarations: [ AppComponent, HelloComponent, TestComponent, AppareilComponent, AuthComponent, AppareilViewComponent ],
   bootstrap:    [ AppComponent ],
   providers: [AppareilService]
 })
