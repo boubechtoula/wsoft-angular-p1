901db64a02441ad6e921bfd29796f1fa569a29ae
boubechtoula <dw_boubechtoula@esi.dz>
2019-10-26 18:55:55 +0100

    correction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,9 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { TestComponent } from './test/test.component';
-import { AppareilComponent } from './appareil/appareil.component';
-
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TestComponent, AppareilComponent ],
+  declarations: [ AppComponent, HelloComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
